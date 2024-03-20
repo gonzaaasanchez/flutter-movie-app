b792f0c2bdf6e4353326e081b2b8f91dca15fb2b
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2024-03-20 18:35:33 -0300

    ADD state notifier

--- lib/app/presentation/modules/sign_in/controller/sign_in_controller.dart
+++ lib/app/presentation/modules/sign_in/controller/sign_in_controller.dart
@@ -1,32 +1,22 @@
-import 'package:flutter/foundation.dart';
-
+import '../../../global/state_notifiers.dart';
 import 'sign_in_state.dart';
 
-class SignInController extends ChangeNotifier {
-  SignInState _state = const SignInState();
-  bool _mounted = true;
-
-  SignInState get state => _state;
-  bool get mounted => _mounted;
+class SignInController extends StateNotifier<SignInState> {
+  SignInController(super.state);
 
   void onUsernameChanged(String text) {
-    _state = _state.copyWith(username: text);
+    onlyUpdate(
+      state.copyWith(username: text),
+    );
   }
 
   void onPasswordChanged(String text) {
-    _state = _state.copyWith(
-      password: text.replaceAll(' ', ''),
+    onlyUpdate(
+      state.copyWith(password: text.replaceAll(' ', '')),
     );
   }
 
   void onFetchingChanged(bool value) {
-    _state = _state.copyWith(fetching: value);
-    notifyListeners();
-  }
-
-  @override
-  void dispose() {
-    _mounted = false;
-    super.dispose();
+    state = state.copyWith(fetching: value);
   }
 }
